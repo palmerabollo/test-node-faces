eb16a0549249a4f33864d3e3ef6c43e80b40167c
Guido García <guido@tid.es>
2013-12-12 00:47:24 +0100

    minor

--- server.js
+++ server.js
@@ -8,40 +8,42 @@ app.configure(function () {
     app.use("/", express.static(__dirname + '/'));
 });
 
-var pending = {};
-
 var server = http.createServer(app).listen(2500);
 var io = require('socket.io').listen(server);
 
+io.set('log level', 1);
+
 io.sockets.on('connection', function (socket) {
-  io.sockets.emit('news', { id: socket.id });
-  
   socket.on('image', function processImage(data) {
-    if (pending[socket.id]) {
-      console.log("pending op");
-      return;
-    }
-
-    pending[socket.id] = true;
-
-    var binaryData = new Buffer(data.image, 'base64');
-    
-    cv.readImage(binaryData, function(err, im) {
+    detectFaces(data.image, function (err, imageData) {
       if (!err) {
-        im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
-            for (var i=0; i<faces.length; i++){
-               var x = faces[i]
-               im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-            }
-            var imageURL = im.toBuffer().toString('base64');
-            io.sockets.emit('image_cv', {image: imageURL});    
-        });
+        io.sockets.emit('image_cv', {id: socket.id, image: imageData});
       }
-
-      delete pending[socket.id];
     });
   });
 });
 
-io.set('log level', 1);
+var detectFaces = function (imageData, callback) {
+    var binaryData = new Buffer(imageData, 'base64');
+    
+    cv.readImage(binaryData, function(err, im) {
+      if (err) {
+        return callback(err);
+      }
 
+      im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+        if (err) {
+          return callback(err);
+        }
+
+        for (var i=0; i<faces.length; i++){
+           var x = faces[i]
+           im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+        }
+
+        var newImageData = im.toBuffer().toString('base64');
+        callback(null, newImageData);
+      });
+    });
+  }
+}
