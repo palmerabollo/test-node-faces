ca01ad60a5aa0b8b1d6dbcfab37b983abe178031
palmerabollo <palmerabollo@gmail.com>
2013-12-11 23:35:35 +0000

    sizes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     http = require('http'),
-    faces = require('faces');
+    cv = require('opencv');
 
 var app = express();
 
 app.configure(function () {
     app.use("/", express.static(__dirname + '/'));
 });
+
+var pending = {};
 
 var server = http.createServer(app).listen(2500);
 var io = require('socket.io').listen(server);
@@ -15,22 +17,31 @@ io.sockets.on('connection', function (socket) {
   io.sockets.emit('news', { id: socket.id });
   
   socket.on('image', function processImage(data) {
+    if (pending[socket.id]) {
+      console.log("pending op");
+      return;
+    }
+
+    pending[socket.id] = true;
+
     var binaryData = new Buffer(data.image, 'base64');
     
-    var cv = require('opencv');
     cv.readImage(binaryData, function(err, im) {
-        im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
-            console.log("faces found: %d", faces.length);
-            for (var i=0;i<faces.length; i++){
+      if (!err) {
+        im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+            for (var i=0; i<faces.length; i++){
                var x = faces[i]
                im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
             }
-            // "data:image/#{fmt};base64,#{buf.toString('base64')}"
-            var imageURL = "data:image/jpg;base64," + im.toBuffer().toString('base64');
+            var imageURL = im.toBuffer().toString('base64');
             io.sockets.emit('image_cv', {image: imageURL});    
-        });    
+        });
+      }
+
+      delete pending[socket.id];
     });
   });
 });
 
-io.set('log level', 0);
+io.set('log level', 1);
+
